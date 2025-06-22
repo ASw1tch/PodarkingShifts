09a52f7e37f4d7c14657a8c2c8b5f8a99e45578b
Gusarov Evgenii <mps.gusarov@crpt.ru>
2025-06-22 16:53:42 +0200

    some fixes and improvements

--- src/main/java/com/telegrambot/TimeValidator.java
+++ src/main/java/com/telegrambot/TimeValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class TimeValidator {
     private static final Pattern pattern = Pattern.compile("^([0-1]?\\d|2[0-3]):[0-5]\\d$");
 
-    public static boolean isValid(String time) {
-        return pattern.matcher(time).matches();
+    public static boolean isInvalid(String time) {
+        return !pattern.matcher(time).matches();
     }
 }
